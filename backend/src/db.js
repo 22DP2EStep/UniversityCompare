b5933888ef808562ef3b770fcc9480c751b9483b
Ernests Stepanovs <es00653@rvt.lv>
2026-05-19 00:01:17 +0300

    bildes lai būtu seeded

--- backend/src/db.js
+++ backend/src/db.js
@@ -146,7 +146,7 @@ async function init() {
 
     const lu = ins('Latvijas Universitāte', 'Rīga', 'https://www.lu.lv',
       'Latvijas lielākā un vecākā augstskola, dibināta 1919. gadā. Piedāvā plašu studiju programmu klāstu dabaszinātnēs, humanitārajās zinātnēs, sociālajās zinātnēs un medicīnā.', 1,
-      'https://upload.wikimedia.org/wikipedia/commons/6/6b/Latvijas_Universit%C4%81tes_centr%C4%81l%C4%81_%C4%93ka.JPG');
+      'https://www.lu.lv/fileadmin/user_upload/lu_portal/projekti/cfi/zinas/2018/csm_J035_6535afd9d9.jpg');
     prog(lu, 'Datorzinātne',       BAK, 3, 2200);
     prog(lu, 'Tiesību zinātne',    BAK, 4, 2000);
     prog(lu, 'Ekonomika',          BAK, 3, 2000);
@@ -158,7 +158,7 @@ async function init() {
 
     const rtu = ins('Rīgas Tehniskā universitāte', 'Rīga', 'https://www.rtu.lv',
       'Vadošā tehniskā augstskola Baltijā, dibināta 1862. gadā. Specializējas inženierzinātnēs, tehnoloģijās un arhitektūrā.', 2,
-      'https://upload.wikimedia.org/wikipedia/commons/2/28/Riga_Tech_Uni.jpg');
+      'https://www.rtu.lv/writable/public_files/b_RTU_1_rtu_kipsala.jpg');
     prog(rtu, 'Datorzinātne un informācijas tehnoloģijas', BAK, 4, 2400);
     prog(rtu, 'Elektrotehnika',                           BAK, 4, 2200);
     prog(rtu, 'Arhitektūra',                              BAK, 4, 2200);
@@ -170,7 +170,7 @@ async function init() {
 
     const rsu = ins('Rīgas Stradiņa universitāte', 'Rīga', 'https://www.rsu.lv',
       'Vadošā medicīnas un veselības zinātņu universitāte Latvijā. Piedāvā studijas medicīnā, zobārstniecībā, farmācijā un sociālajās zinātnēs.', 3,
-      'https://upload.wikimedia.org/wikipedia/commons/6/6c/Rigas_Stradina_universitate.jpg');
+      'https://www.researchlatvia.gov.lv/sites/default/files/styles/article_embed_image/public/2024-06/rsu.jpg?itok=O9Pka1Iz');
     prog(rsu, 'Medicīna',           DOK, 6, 8000, 'Latviešu/Angļu');
     prog(rsu, 'Zobārstniecība',     DOK, 5, 7000, 'Latviešu/Angļu');
     prog(rsu, 'Farmācija',          BAK, 4, 3000);
@@ -180,7 +180,7 @@ async function init() {
 
     const lbtu = ins('Latvijas Biozinātņu un tehnoloģiju universitāte', 'Jelgava', 'https://www.lbtu.lv',
       'Agrāk zināma kā Latvijas Lauksaimniecības universitāte. Specializējas lauksaimniecībā, pārtikas zinātnē, mežsaimniecībā un vides zinātnēs.', 4,
-      'https://upload.wikimedia.org/wikipedia/commons/d/df/JelgavaPalace2024.jpg');
+      'https://upload.wikimedia.org/wikipedia/commons/a/a9/Biron_Rezidenz.jpg');
     prog(lbtu, 'Lauksaimniecība',              BAK, 4, 1600);
     prog(lbtu, 'Pārtikas zinātne un tehnoloģija', BAK, 4, 1800);
     prog(lbtu, 'Mežzinātne',                   BAK, 4, 1600);
@@ -190,7 +190,7 @@ async function init() {
 
     const du = ins('Daugavpils Universitāte', 'Daugavpils', 'https://www.du.lv',
       'Reģionālā universitāte Austrumlatvijā, kas piedāvā studijas humanitārajās, sociālajās un dabaszinātnēs. Daudzvalodu vide — latviešu un krievu mācībvaloda.', 5,
-      'https://upload.wikimedia.org/wikipedia/commons/e/e3/Daugavpils_University.JPG');
+      'https://du.lv/wp-content/uploads/2023/10/DJI_0041-scaled.jpg');
     prog(du, 'Pedagoģija',       BAK, 4, 1400);
     prog(du, 'Angļu filoloģija', BAK, 3, 1400);
     prog(du, 'Bioloģija',        BAK, 3, 1400);
@@ -199,14 +199,14 @@ async function init() {
 
     const liepu = ins('Liepājas Universitāte', 'Liepāja', 'https://www.liepu.lv',
       'Universitāte Latvijas rietumu piekrastē. Specializējas pedagoģijā, mūzikā, mākslā un humanitārajās zinātnēs.', 6,
-      'https://upload.wikimedia.org/wikipedia/commons/0/04/Liep%C4%81ja_-_university.jpg');
+      'https://upload.wikimedia.org/wikipedia/commons/c/c0/Liep%C4%81jas_Pedago%C4%A3ijas_akad%C4%93mija_1999-09-03_-_panoramio.jpg');
     prog(liepu, 'Pedagoģija',    BAK, 4, 1400);
     prog(liepu, 'Mūzika',        BAK, 4, 1500);
     prog(liepu, 'Sociālais darbs', BAK, 3, 1300);
 
     const via = ins('Vidzemes Augstskola', 'Valmiera', 'https://www.va.lv',
       'Reģionālā augstskola Vidzemē. Specializējas komunikācijā, informācijas tehnoloģijās, tūrismā un uzņēmējdarbībā.', 7,
-      'https://upload.wikimedia.org/wikipedia/commons/3/3e/Vidzeme_University_of_Applied_Sciences.jpg');
+      'https://www.researchlatvia.gov.lv/sites/default/files/styles/article_embed_image/public/2024-06/ViA.jpg?itok=8fzGQHSy');
     prog(via, 'Informācijas tehnoloģijas', BAK, 3, 1600);
     prog(via, 'Komunikācija',             BAK, 3, 1500);
     prog(via, 'Tūrisma vadība',           BAK, 3, 1500);
@@ -214,34 +214,34 @@ async function init() {
 
     const rta = ins('Rēzeknes Tehnoloģiju akadēmija', 'Rēzekne', 'https://www.rta.lv',
       'Tehnoloģiju un mākslas akadēmija Latgalē. Piedāvā studijas inženierzinātnēs, pedagoģijā un mākslā.', 8,
-      'https://upload.wikimedia.org/wikipedia/commons/e/eb/Inzenieru_fak_Rezekne.jpg');
+      'https://www.rtu.lv/writable/public_files/b_RTU_2_rtu_rezekne.jpg.webp');
     prog(rta, 'Inženiermehānika', BAK, 4, 1400);
     prog(rta, 'Pedagoģija',       BAK, 4, 1300);
     prog(rta, 'Dizains',          BAK, 4, 1500);
 
     const lma = ins('Latvijas Mākslas akadēmija', 'Rīga', 'https://www.lma.lv',
       'Latvijas vadošā vizuālās mākslas augstskola, dibināta 1919. gadā. Piedāvā studijas glezniecībā, grafikā, dizainā, skulptūrā un mākslas teorijā.', 9,
-      'https://upload.wikimedia.org/wikipedia/commons/9/9e/Latvijas_M%C4%81kslas_akad%C4%93mija2.jpg');
+      'https://enciklopedija.lv/api/image/thumbnail?name=a3692a10b0e7-6bc8f992-4958-442c-8198-651e616ccb0b.jpg&size=title');
     prog(lma, 'Glezniecība',      BAK, 4, 1800);
     prog(lma, 'Grafiskais dizains', BAK, 4, 1800);
     prog(lma, 'Skulptūra',        BAK, 4, 1800);
 
     const jvlma = ins('Jāzepa Vītola Latvijas Mūzikas akadēmija', 'Rīga', 'https://www.jvlma.lv',
       'Latvijas valsts mūzikas augstskola, dibināta 1919. gadā. Augstas klases muzikālā izglītība vijolē, klavierēs, diriģēšanā, kompozīcijā un citās disciplīnās.', 10,
-      'https://upload.wikimedia.org/wikipedia/commons/8/84/J%C4%81zeps_V%C4%ABtols_Latvian_Academy_of_Music.JPG');
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTokNl4RoOfmThH77qAtEAsYiG_0bZ9a5wRbg&s');
     prog(jvlma, 'Klavierspēle', BAK, 4, 1700);
     prog(jvlma, 'Diriģēšana',   BAK, 4, 1700);
     prog(jvlma, 'Kompozīcija',  BAK, 4, 1700);
 
     const lspa = ins('Latvijas Sporta pedagoģijas akadēmija', 'Rīga', 'https://www.lspa.lv',
       'Specializēta augstskola sporta zinātnē, fiziskajā audzināšanā un veselības izglītībā.', 11,
-      'https://upload.wikimedia.org/wikipedia/commons/c/c9/LSPA_%C4%93ka.jpg');
+      'https://enciklopedija.lv/api/image/thumbnail?name=d996465d5be9-86ef50d0-e6cc-41ca-9ad4-7a9d307bfb66.jpg&size=title');
     prog(lspa, 'Sports un fiziskā audzināšana', BAK, 4, 1500);
     prog(lspa, 'Sporta zinātne',                BAK, 4, 1500);
 
     const riseba = ins('RISEBA Biznesa, mākslas un tehnoloģiju augstskola', 'Rīga', 'https://www.riseba.lv',
       'Starptautiska privātā augstskola, kas piedāvā biznesa administrācijas, komunikācijas, arhitektūras un IT studijas angļu un latviešu valodā.', 12,
-      'https://upload.wikimedia.org/wikipedia/commons/5/55/RISEBA%2C_biznesa%2C_m%C4%81kslas_un_tehnolo%C4%A3iju_augstskola%2C_galven%C4%81_%C4%93ka.jpg');
+      'https://riseba.lv/wp-content/uploads/2023/08/risebaeka.jpg');
     prog(riseba, 'Biznesa administrācija', BAK, 3, 3200, 'Latviešu/Angļu');
     prog(riseba, 'Komunikācija',           BAK, 3, 3000, 'Latviešu/Angļu');
     prog(riseba, 'Arhitektūra',            BAK, 4, 3400, 'Angļu');
@@ -249,7 +249,7 @@ async function init() {
 
     const ba = ins('Banku augstskola', 'Rīga', 'https://www.ba.lv',
       'Specializēta finanšu un banku augstskola. Piedāvā studijas finanšu vadībā, grāmatvedībā un uzņēmējdarbības analīzē.', 13,
-      'https://upload.wikimedia.org/wikipedia/commons/8/86/Banku_augstskola_-_panoramio.jpg');
+      'https://www.lu.lv/fileadmin/_processed_/6/a/csm_Banku_augstskola_67e3aace8a.jpg');
     prog(ba, 'Finanšu vadība',         BAK, 3, 2800);
     prog(ba, 'Grāmatvedība',           BAK, 3, 2600);
     prog(ba, 'Uzņēmējdarbības analīze', BAK, 3, 2700);
@@ -257,28 +257,29 @@ async function init() {
 
     const tsi = ins('Transporta un sakaru institūts', 'Rīga', 'https://www.tsi.lv',
       'Augstskola ar specializāciju transporta, loģistikas, IT un telekomunikāciju jomā. Piedāvā studijas krievu, latviešu un angļu valodā.', 14,
-      'https://upload.wikimedia.org/wikipedia/commons/4/43/Logo_of_Transport_and_Telecommunication_Institute.png');
+      'https://www.lk-group.lv/site-files/gallery/2020-06/1591340400_7b8e2c38293a7b22f00afb5a50df09ff.jpg');
     prog(tsi, 'Datorzinātnes',                       BAK, 4, 2200, 'Latviešu/Krievu');
     prog(tsi, 'Loģistika un piegādes ķēžu vadība',  BAK, 4, 2000, 'Latviešu/Krievu');
     prog(tsi, 'Aviācijas transports',                BAK, 4, 2400, 'Angļu');
 
     const sse = ins('SSE Rīga (Rīgas Juridiskā augstskola)', 'Rīga', 'https://www.sseriga.edu',
       'Augsta līmeņa biznesa augstskola, kas saistīta ar Stokholmas Ekonomikas augstskolu. Pilnībā angļu valodā. Ļoti konkurētspējīga uzņemšana.', 15,
-      'https://upload.wikimedia.org/wikipedia/commons/3/3b/Strelnieku_iela_4a.JPG');
+      'https://i.jauns.lv/t/2024/11/11/3162506/490x350.webp?v=1731325007');
     prog(sse, 'Ekonomika',      BAK, 3, 8500, 'Angļu');
     prog(sse, 'Biznesa vadība', BAK, 3, 8500, 'Angļu');
     prog(sse, 'Ekonomika',      MAG, 2, 9500, 'Angļu');
 
     const turiba = ins('Biznesa augstskola Turība', 'Rīga', 'https://www.turiba.lv',
       'Privāta biznesa augstskola ar plašu studiju programmu klāstu biznesa vadībā, tūrismā, tiesībās un komunikācijā.', 16,
-      'https://upload.wikimedia.org/wikipedia/commons/0/02/Turiba_University_Hostel.jpg');
+      'https://www.turiba.lv/storage/files/biznesa-augstskola-turiba.jpg');
     prog(turiba, 'Biznesa vadība',  BAK, 3, 2500);
     prog(turiba, 'Tiesību zinātne', BAK, 4, 2500);
     prog(turiba, 'Tūrisma vadība',  BAK, 3, 2300);
     prog(turiba, 'Biznesa vadība',  MAG, 2, 2800);
 
     const lka = ins('Latvijas Kristīgā akadēmija', 'Rīga', 'https://www.lka.edu.lv',
-      'Privāta augstskola ar kristīgu ievirzi, kas piedāvā studijas teoloģijā, sociālajā darbā un pedagoģijā.', 17);
+      'Privāta augstskola ar kristīgu ievirzi, kas piedāvā studijas teoloģijā, sociālajā darbā un pedagoģijā.', 17,
+      'https://viss.lv/dati/latvijas_kristiga_akademija/logo_latvijas_kristiga_akademija.jpg');
     prog(lka, 'Teoloģija',      BAK, 4, 1200);
     prog(lka, 'Sociālais darbs', BAK, 3, 1200);
 
